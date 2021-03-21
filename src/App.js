6329a473724cb41a515588b98ddc2d6db772584e
Taha <mt.zngn@gmail>
2021-03-21 18:21:21 +0000

    functions of sorting alghorithms connected to the buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Navbar from "./components/Navbar";
 import Visualizer from "./components/Visualizer"; 
 import Control from "./components/Control"; 
-import randomArrCreate from "./utils/";
+import { randomArrCreate } from "./utils/";
 
 const StyledApp = styled.div`
 
@@ -15,35 +15,11 @@ background-color: #8ac4d0;
 function App() {
   const [numArr, setNumArr] = useState(randomArrCreate());
   
-  const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
 
-  const insertionSort = async(list) => {
-    let len = list.length;
-
-    for(let i = 1; i < len +1; i++){
-
-        for(let j = i; j > 0; j--){
-            if(list[j] < list[j-1]){
-                let temp = list[j - 1];
-                list[j-1] = list[j];
-                list[j] = temp
-                await sleep(50);
-                setNumArr(list)
-            }
-        }
-    
-    }
-    console.log(list);
-}
-
-
+ 
   return (
     <StyledApp>
-      <Navbar/>
-      <button onClick={()=>insertionSort([...numArr])}>test</button>
+      <Navbar numArr={numArr} setNumArr={setNumArr}/>
       <Visualizer numArr={numArr}/>
       <Control/>
     </StyledApp>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-const randomArrCreate = () => {
+export const randomArrCreate = () => {
     let randomArr = [];
 
     while(randomArr.length < 60){
@@ -8,7 +8,80 @@ const randomArrCreate = () => {
     return randomArr;
 }
 
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+
+export const insertionSort = async(list, setNumArr) => {
+    let len = list.length;
+
+    for(let i = 1; i < len +1; i++){
+
+        for(let j = i; j > 0; j--){
+            if(list[j] < list[j-1]){
+                let temp = list[j - 1];
+                list[j-1] = list[j];
+                list[j] = temp;
+                await sleep(100);
+                setNumArr(list);
+            }
+        }    
+    }
+}
+export const bubbleSort = async(list, setNumArr) => {
+    for(let j =0; j < list.length ; j++){
+        for(let i = 0; i < list.length ; i++){
+            if(list[i] > list[i+1]){
+                let temp = list[i+1];
+                list[i+1] = list[i];
+                list[i] = temp;
+                await sleep(100);
+                setNumArr(list);
+            }
+
+        }
+    }
+}
+
+export const selectionSort = async(list, setNumArr) => {
+    let len = list.length;
+
+for(let i = 0; i < len; i++){
+    for(let j = i; j < len; j++){
+        if(list[i] > list[j]){
+            let temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+            await sleep(100);
+            setNumArr(list);
+        }
+    }
+}
+}
+
+const merge = (left, right) => {
+    let arr = []
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [ ...arr, ...left, ...right ];
+}
+
+export const mergeSort = (list) => {
+    const half = list.length / 2;
+    
+    if(list.length < 2){
+      return list; 
+    }
+    
+    const left = list.splice(0, half);
+    return merge(mergeSort(left),mergeSort(list));
+  }
 
 
 
-export default randomArrCreate;
